adf7ead1085f2c0f4350cdd2f7b982191dd3d946
san <upsu90@gmail.com>
2021-06-28 15:33:14 +0200

    Add global styles for FruitIngredients

--- components/FruitIngredientRight.jsx
+++ components/FruitIngredientRight.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Image from "next/image";
-import styles from "../components/FruitIngredientRight.module.css";
+import styles from "../styles/FruitIngredient.module.css";
+import stylesIngredient from "../components/FruitIngredientRight.module.css";
 
 export default function FruitIngredients(props) {
   return (
-    <button className={styles.buttonFruit}>
+    <button className={`${styles.buttonFruit} ${stylesIngredient.buttonFruit}`}
+    >
       <Image src={props.imageSource} alt="" width={120} height={120} />
       <span className={styles.buttonName}>{props.fruitName}</span>
     </button>
