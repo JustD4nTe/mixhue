31bdc1152a93698e87af14eddf2844f763aad443
Dominik Tyc <domikxf@gmail.com>
2021-06-29 11:24:51 +0200

    Add fruits into mixer

--- components/Mixer.jsx
+++ components/Mixer.jsx
@@ -11,6 +11,33 @@ export default function Mixer() {
         width={240}
         height={520}
       />
+      <div className={styles.insideMixer}>
+        <div className={`${styles.thirdFruit} ${styles.fruitInMixer}`}>
+          <Image
+            src={`/images/fruits/kiwi.svg`}
+            alt=""
+            width={100}
+            height={100}
+          />
+        </div>
+        <div className={`${styles.secondFruit} ${styles.fruitInMixer}`}>
+          <Image
+            src={`/images/fruits/kiwi.svg`}
+            alt=""
+            width={100}
+            height={100}
+          />
+        </div>
+
+        <div className={`${styles.firstFruit} ${styles.fruitInMixer}`}>
+          <Image
+            src={`/images/fruits/kiwi.svg`}
+            alt=""
+            width={100}
+            height={100}
+          />
+        </div>
+      </div>
     </div>
   );
 }
